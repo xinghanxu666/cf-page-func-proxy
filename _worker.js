70b2e99144826e385087cb885cd4a5a12defa2c1
星涵煦 <110706797+xinghanxu666@users.noreply.github.com>
2024-08-15 16:04:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname=hoatname;
+        url.hostname=DAEMON;
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
